2b0da005d314a8ee86d23ea5681826fbe44a9f18
ivanmslima <81934424+ivanmslima@users.noreply.github.com>
2022-11-14 21:57:23 -0300

    Adicionar arquivos de projeto.

---
+++ EstruturaDeRepeticaoForPara/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+
+namespace curso
+{
+    class Progam
+    {
+        static void Main(string[] args)
+        {
+            int N, x, soma;
+
+            N = int.Parse(Console.ReadLine());
+
+            soma = 0;
+            for (int i = 1; i <= N; i++)
+            {
+                x = int.Parse(Console.ReadLine());
+                
+                
+                soma = soma + x;
+            }
+            Console.WriteLine(soma);
+            Console.WriteLine();
+
+
+
+        }
+    }
+}
